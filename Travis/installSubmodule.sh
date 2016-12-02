41f517891a852c07c7e3dcdbe79bcf92c52a86ec
Ajiemar Santiago <ajiemar@gmail.com>
2016-12-02 15:12:37 -0600

    tracing directory

--- Travis/installSubmodule.sh
+++ Travis/installSubmodule.sh
@@ -1,5 +1,14 @@
 echo "Attempting to install the base .NET SDK into the sdk..."
 echo $(pwd)
+echo $(ls)
+
+cd Test/Test
+echo $(ls)
+
+cd bin/Release
+echo $(ls)
+echo $(ls -l)
+
 git clone -b feature-dotNetSDKChanges https://github.com/mediumTaj/csharp-sdk-base.git /home/travis/build/mediumTaj/dot-net-sdk/sdk/src/services
 if [ $? = 0 ] ; then
   echo "Base .NET SDK install SUCCEEDED! Exited with $?"
