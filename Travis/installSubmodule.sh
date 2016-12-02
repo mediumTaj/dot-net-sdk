d3d19354ce8206c7ecc808b9ecca34a6e4640f64
Ajiemar Santiago <ajiemar@gmail.com>
2016-12-02 11:37:57 -0600

    path to test, tracing out path

--- Travis/installSubmodule.sh
+++ Travis/installSubmodule.sh
@@ -1,5 +1,5 @@
 echo "Attempting to install the base .NET SDK into the sdk..."
-
+echo $(pwd)
 git clone -b feature-dotNetSDKChanges https://github.com/mediumTaj/csharp-sdk-base.git /home/travis/build/mediumTaj/dot-net-sdk/sdk/src/services
 if [ $? = 0 ] ; then
   echo "Base .NET SDK install SUCCEEDED! Exited with $?"
