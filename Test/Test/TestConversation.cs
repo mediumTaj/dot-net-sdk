053cbcefac1e1118cb2fad9322998f58b594b7fa
Ajiemar Santiago <ajiemar@gmail.com>
2016-12-06 11:24:31 -0600

    Conversation integration test

---
+++ Test/Test/TestConversation.cs
@@ -0,0 +1,61 @@
+﻿
+
+
+/**
+* Copyright 2015 IBM Corp. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+using IBM.Watson.DeveloperCloud.Services.Conversation.v1;
+using IBM.Watson.DeveloperCloud.Utilities;
+using NUnit.Framework;
+namespace sdk.test
+{
+  [TestFixture]
+  class TestConversation
+  {
+    Conversation conversation = new Conversation();
+    private string workspaceID;
+    private string input = "Can you unlock the door?";
+
+    [Test]
+    private void TestMessageObject()
+    {
+      if (Config.Instance.FindCredentials(conversation.GetServiceID()) == null)
+        Assert.Fail("Failed to find credentials");
+
+      if (!conversation.Message((MessageResponse resp, string data) =>
+      {
+        Assert.AreNotEqual(resp, null);
+      }, workspaceID, input))
+        Assert.Fail("Failed to send message! {0}", input);
+    }
+
+    [Test]
+    private void TestMessageInput()
+    {
+      MessageRequest messageRequest = new MessageRequest();
+      messageRequest.InputText = input;
+
+      if (Config.Instance.FindCredentials(conversation.GetServiceID()) == null)
+        Assert.Fail("Failed to find credentials");
+
+      if (!conversation.Message((MessageResponse resp, string data) =>
+      {
+        Assert.AreNotEqual(resp, null);
+      }, workspaceID, messageRequest))
+        Assert.Fail("Failed to send message! {0}", messageRequest.input);
+    }
+  }
+}
