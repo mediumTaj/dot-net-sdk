c974183beebf71200828e24da11e0217dc65224a
Ajiemar Santiago <ajiemar@gmail.com>
2016-12-13 17:16:37 -0600

    Speech To Text integration tests

--- Test/Test/TestSpeechToText.cs
+++ Test/Test/TestSpeechToText.cs
@@ -18,6 +18,9 @@
 using NUnit.Framework;
 using IBM.Watson.DeveloperCloud.Services.SpeechToText.v1;
 using IBM.Watson.DeveloperCloud.Logging;
+using System.IO;
+using System;
+using System.Collections.Generic;
 
 namespace sdk.test
 {
@@ -25,8 +28,9 @@ namespace sdk.test
   {
     private SpeechToText speechToText = new SpeechToText();
     private string createdCustomizationID;
-    private string createCustomizationName = "dotnet-integration-test-customization";
+    private string createdCustomizationName = "dotnet-integration-test-customization";
     private string createdCorpusName = "dotnet-integration-test-corpus";
+    private string createdCustomizationDescription = "dotnet integration test speech to text customization.";
     private string customCorpusFilePath;
     private string speechToTextModelEnglish = "en-US_BroadbandModel";
     private string customWordsFilePath;
@@ -51,97 +55,286 @@ namespace sdk.test
         Assert.NotNull(models);
         autoEvent.Set();
       }))
-        Assert.Fail("Failed to invoke GetModels.");
+        Assert.Fail("Failed to invoke GetModels();");
+
+      autoEvent.WaitOne();
     }
 
-    //[Test]
-    //public void SpeechToText_TestGetModel()
-    //{
+    [Test]
+    public void SpeechToText_TestGetModel()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to get model {0}...", speechToTextModelEnglish);
 
-    //}
+      if (!speechToText.GetModel((Model model) =>
+      {
+        Assert.NotNull(model);
+        autoEvent.Set();
+      }, speechToTextModelEnglish))
+        Assert.Fail("Failed to invoke GetModel();");
 
-    //[Test]
-    //public void SpeechToText_TestGetCustomizations()
-    //{
+      autoEvent.WaitOne();
+    }
 
-    //}
+    [Test]
+    public void SpeechToText_TestGetCustomizations()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to get customizations...");
 
-    //[Test]
-    //public void SpeechToText_TestCreateCustomizations()
-    //{
+      if (!speechToText.GetCustomizations((Customizations customizations, string data) =>
+      {
+        Assert.NotNull(customizations);
+        autoEvent.Set();
+      }))
+        Assert.Fail("Failed to invoke GetCustomizations();");
 
-    //}
+      autoEvent.WaitOne();
+    }
 
-    //[Test]
-    //public void SpeechToText_TestGetCustomization()
-    //{
+    [Test, Order(5)]
+    public void SpeechToText_TestCreateCustomization()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to create customization...");
 
-    //}
+      if (!speechToText.CreateCustomization((CustomizationID customizationID, string data) =>
+      {
+        createdCustomizationID = customizationID.customization_id;
+        Assert.NotNull(customizationID);
+        autoEvent.Set();
+      }, createdCustomizationName, speechToTextModelEnglish, createdCustomizationDescription))
+        Assert.Fail("Failed to invoke CreateCustomization();");
 
-    //[Test]
-    //public void SpeechToText_TestGetCustomCorpora()
-    //{
+      autoEvent.WaitOne();
+    }
 
-    //}
+    [Test, Order(6)]
+    public void SpeechToText_TestGetCustomization()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to get customization {0}...", createdCustomizationID);
 
-    //[Test]
-    //public void SpeechToText_TestAddCustomCorpus()
-    //{
+      if (!speechToText.GetCustomization((Customization customization, string data) =>
+      {
+        Assert.NotNull(customization);
+        autoEvent.Set();
+      }, createdCustomizationID))
+        Assert.Fail("Failed to invoke GetCustomization();");
 
-    //}
+      autoEvent.WaitOne();
+    }
 
-    //[Test]
-    //public void SpeechToText_TestGetCustomWords()
-    //{
+    [Test]
+    public void SpeechToText_TestGetCustomCorpora()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to get custom corpora...");
 
-    //}
+      if (!speechToText.GetCustomCorpora((Corpora corpora, string data) =>
+      {
+        Assert.NotNull(corpora);
+        autoEvent.Set();
+      }, createdCustomizationID))
+        Assert.Fail("Failed to invoke GetCustomCorpora();");
 
-    //[Test]
-    //public void SpeechToText_TestAddCustomWordsUsingFile()
-    //{
+      autoEvent.WaitOne();
+    }
 
-    //}
+    [Test, Order(7)]
+    public void SpeechToText_TestAddCustomCorpusUsingFile()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to add custom corpus using file...");
 
-    //[Test]
-    //public void SpeechToText_TestAddCustomWordsUsingObject()
-    //{
+      if (!speechToText.AddCustomCorpus((bool success, string data) =>
+      {
+        Assert.True(success);
+        autoEvent.Set();
+      }, createdCustomizationID, createdCorpusName, allowOverwrite, customCorpusFilePath))
+        Assert.Fail("Failed to invoke AddCustomCorpus(); using file");
 
-    //}
+      autoEvent.WaitOne();
+    }
 
-    //[Test]
-    //public void SpeechToText_TestGetCustomWord()
-    //{
+    [Test, Order(8)]
+    public void SpeechToText_TestAddCustomCorpusUsingData()
+    {
+      byte[] customCorpusData = null ;
 
-    //}
+      Log.Debug("TestSpeechToText", "Attempting to add custom corpus using data...");
+      try
+      {
+        customCorpusData = File.ReadAllBytes(customCorpusFilePath);
+      }
+      catch(Exception e)
+      {
+        Assert.Fail("Failed to read file. " + e.Message);
+        autoEvent.Set();
+      }
 
-    //[Test]
-    //public void SpeechToText_TestTrainCustomization()
-    //{
+      if (customCorpusData != null)
+      {
+        if (!speechToText.AddCustomCorpus((bool success, string data) =>
+        {
+          Assert.True(success);
+          autoEvent.Set();
+        }, createdCustomizationID, createdCorpusName, allowOverwrite, customCorpusData))
+          Assert.Fail("Failed to invoke AddCustomCorpus(); using data");
+      }
 
-    //}
+      autoEvent.WaitOne();
+    }
 
-    //[Test]
-    //public void SpeechToText_TestDeleteCustomCorpus()
-    //{
+    [Test]
+    public void SpeechToText_TestGetCustomWords()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to get custom words...");
 
-    //}
+      if (!speechToText.GetCustomWords((WordsList wordList, string data) =>
+      {
+        Assert.NotNull(wordList);
+        autoEvent.Set();
+      }, createdCustomizationID))
+        Assert.Fail("Failed to invoke GetCustomWords();");
 
-    //[Test]
-    //public void SpeechToText_TestDeleteCustomWord()
-    //{
+      autoEvent.WaitOne();
+    }
 
-    //}
+    [Test, Order(9)]
+    public void SpeechToText_TestAddCustomWordsUsingFile()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to add custom words using file...");
 
-    //[Test]
-    //public void SpeechToText_TestResetCustomization()
-    //{
+      if (!speechToText.AddCustomWords((bool success, string data) =>
+      {
+        Assert.True(success);
+        autoEvent.Set();
+      }, createdCustomizationID, customWordsFilePath))
+        Assert.Fail("Failed to invoke AddCustomWords(); file");
 
-    //}
+      autoEvent.WaitOne();
+    }
 
-    //[Test]
-    //public void SpeechToText_TestDeleteCustomization()
-    //{
+    [Test, Order(10)]
+    public void SpeechToText_TestAddCustomWordsUsingObject()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to add custom words using object...");
 
-    //}
+      Words words = new Words();
+      Word w0 = new Word();
+      List<Word> wordList = new List<Word>();
+      w0.word = "mikey";
+      w0.sounds_like = new string[1];
+      w0.sounds_like[0] = "my key";
+      w0.display_as = "Mikey";
+      wordList.Add(w0);
+      Word w1 = new Word();
+      w1.word = "charlie";
+      w1.sounds_like = new string[1];
+      w1.sounds_like[0] = "char lee";
+      w1.display_as = "Charlie";
+      wordList.Add(w1);
+      Word w2 = new Word();
+      w2.word = "bijou";
+      w2.sounds_like = new string[1];
+      w2.sounds_like[0] = "be joo";
+      w2.display_as = "Bijou";
+      wordList.Add(w2);
+      words.words = wordList.ToArray();
+
+      if (!speechToText.AddCustomWords((bool success, string data) =>
+      {
+        Assert.True(success);
+        autoEvent.Set();
+      }, createdCustomizationID, words))
+        Assert.Fail("Failed to invoke AddCustomWords(); object");
+
+      autoEvent.WaitOne();
+    }
+
+    [Test, Order(11)]
+    public void SpeechToText_TestGetCustomWord()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to get custom word...");
+
+      if (!speechToText.GetCustomWord((WordData word, string data) =>
+      {
+        Assert.NotNull(word);
+        autoEvent.Set();
+      }, createdCustomizationID, wordToGet))
+        Assert.Fail("Failed to invoke GetCustomWord();");
+
+      autoEvent.WaitOne();
+    }
+
+    [Test, Order(12)]
+    public void SpeechToText_TestTrainCustomization()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to get train customization...");
+
+      if (!speechToText.TrainCustomization((bool success, string data) =>
+      {
+        Assert.True(success);
+        autoEvent.Set();
+      }, createdCustomizationID))
+        Assert.Fail("Failed to invoke TrainCustomization();");
+
+      autoEvent.WaitOne();
+    }
+
+    [Test, Order(13)]
+    public void SpeechToText_TestDeleteCustomCorpus()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to delete custom corpus...");
+
+      if (!speechToText.DeleteCustomCorpus((bool success, string data) =>
+      {
+        Assert.True(success);
+        autoEvent.Set();
+      }, createdCustomizationID, createdCorpusName))
+        Assert.Fail("Failed to invoke DeleteCustomCorpus();");
+
+      autoEvent.WaitOne();
+    }
+
+    [Test, Order(14)]
+    public void SpeechToText_TestDeleteCustomWord()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to delete custom word...");
+
+      if (!speechToText.DeleteCustomWord((bool success, string data) =>
+      {
+        Assert.True(success);
+        autoEvent.Set();
+      }, createdCustomizationID, wordToGet))
+        Assert.Fail("Failed to invoke DeleteCustomWord();");
+
+      autoEvent.WaitOne();
+    }
+
+    [Test, Order(15)]
+    public void SpeechToText_TestResetCustomization()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to reset customization...");
+
+      if (!speechToText.ResetCustomization((bool success, string data) =>
+      {
+        Assert.True(success);
+        autoEvent.Set();
+      }, createdCustomizationID))
+        Assert.Fail("Failed to invoke ResetCustomization();");
+
+      autoEvent.WaitOne();
+    }
+
+    [Test, Order(16)]
+    public void SpeechToText_TestDeleteCustomization()
+    {
+      Log.Debug("TestSpeechToText", "Attempting to delete customization...");
+
+      if (!speechToText.DeleteCustomization((bool success, string data) =>
+      {
+        Assert.True(success);
+        autoEvent.Set();
+      }, createdCustomizationID))
+        Assert.Fail("Failed to invoke DeleteCustomization();");
+
+      autoEvent.WaitOne();
+    }
   }
 }
