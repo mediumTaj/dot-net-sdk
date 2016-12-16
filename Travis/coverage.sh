59f5d2815b2a3fe985e925a3f75ece5db7ed34cb
Ajiemar Santiago <ajiemar@gmail.com>
2016-12-16 10:53:55 -0600

    revising directory for tests

--- Travis/coverage.sh
+++ Travis/coverage.sh
@@ -18,8 +18,8 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   nuget install -Verbosity quiet -OutputDirectory packages -Version 4.6.519 OpenCover
   nuget install -Verbosity quiet -OutputDirectory packages -Version 2.4.5.0 ReportGenerator
 
-  OPENCOVER=$PWD/packages/OpenCover.4.6.519/tools/OpenCover.Console.exe
-  REPORTGENERATOR=$PWD/packages/ReportGenerator.2.4.5.0/tools/ReportGenerator.exe
+  OPENCOVER=Test/packages/OpenCover.4.6.519/tools/OpenCover.Console.exe
+  REPORTGENERATOR=Test/packages/ReportGenerator.2.4.5.0/tools/ReportGenerator.exe
 
   echo "Calculating coverage with OpenCover..."
   $OPENCOVER \
--- Travis/coveralls.sh
+++ Travis/coveralls.sh
@@ -8,7 +8,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   if [ -n "$COVERALLS_REPO_TOKEN" ]
   then
     nuget install -OutputDirectory packages -Version 0.7.0 coveralls.net
-    packages/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i reports/results.xml --serviceName "travis-ci" --useRelativePaths
+    Test/packages/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i reports/results.xml --serviceName "travis-ci" --useRelativePaths
   fi
 
 else
