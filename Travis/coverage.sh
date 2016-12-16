1545b28c7083eff8dc6c85531f5741cc43b71076
Ajiemar Santiago <ajiemar@gmail.com>
2016-12-16 11:18:23 -0600

    add execute rights to opencover and reportgenerator, commenting out tests for now

--- Travis/coverage.sh
+++ Travis/coverage.sh
@@ -11,8 +11,8 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   echo "Decrypting config..."
   openssl aes-256-cbc -K $encrypted_89fb597d004e_key -iv $encrypted_89fb597d004e_iv -in Config.json.enc -out Test/Test/bin/Release/appdata/Config.json -d
 
-  echo "Running dot-net-sdk integration Tests..."
-  mono ./NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe Test/Test/bin/Release/Test.dll --result=reports/TestResults.xml
+  #echo "Running dot-net-sdk integration Tests..."
+  #mono ./NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe Test/Test/bin/Release/Test.dll --result=reports/TestResults.xml
 
   # Install OpenCover and ReportGenerator, and save the path to their executables.
   nuget install -Verbosity quiet -OutputDirectory packages -Version 4.6.519 OpenCover
@@ -20,6 +20,15 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 
   OPENCOVER=Test/packages/OpenCover.4.6.519/tools/OpenCover.Console.exe
   REPORTGENERATOR=Test/packages/ReportGenerator.2.4.5.0/tools/ReportGenerator.exe
+
+  chmod +x $OPENCOVER
+  chmod +x $REPORTGENERATOR
+
+  # echo "Attempting mono call for opencover"
+  # mono ./Test/packages/OpenCover.4.6.519/tools/OpenCover.Console.exe -target:"Test/packages/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe" -targetargs:"Test/Test/bin/Release/Test.dll --result=reports/TestResults.xml" -output:reports/results.xml -mergeoutput -hideskipped:File -oldStyle -skipautoprops -excludebyfile:*DataModels.cs -filter:"+[*]* -[Test]* -[sdk]FullSerializer.* -[sdk]MiniJSON.*" -searchdirs:$testdir/bin/$CONFIG/netcoreapp1.0 -register:user
+  #
+  # echo "Attempting mono call for report generator"
+
 
   echo "Calculating coverage with OpenCover..."
   $OPENCOVER \
